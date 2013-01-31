cae82347323f3ac0a8bd6ac207123eeaa416aa1e
naohta <naohta@gmail.com>
2013-01-31 15:04:05 +0900

    Partial... but WORK! NAAjax2 js library is good now.

--- web.rb
+++ web.rb
@@ -1,90 +1,45 @@
 #coding:utf-8
 require 'sinatra'
-def load_backyard
-  load './dynamo_read.rb'
-  load './dynamo_write.rb'
-  load './dynamo_auth.rb'
-end
-load_backyard
-
-get '/' do "Hello! :) I'm made by Naohiro OHTA" end
+require './dynamo_read.rb'
+require './dynamo_write.rb'
+require './dynamo_auth.rb'
 
 before do
-  content_type :json
+  content_type:json
 end
 
+get '/' do "Hello! :)   I'm kuwai, made by Naohiro OHTA" end
 
-get '/read' do read_table_names end
-get '/load' do load_backyard; "Backyard programs are loaded." end
-get '/time' do time end
-
-
-get '/template/*' do |title| template title end
-
-
-#------------------------------------------
-get '/create/*/*' do |tenant,title|
-  create_record(tenant,title)
-end
-
-get '/list/*/*/*' do |tenant,range_begins_with,verify_count|
-  if(verify_by_count(count, [tenant,range_begins_with])) then
-    return list_records(tenant,range_begins_with)
-  else
-    return "Verify by counting is failed..."
-  end
-end
-
-
-
-get '/one/*/*' do |tenant,range_value|
-  one tenant,range_value
-end
-#------------------------------------------
-
-def pad(callback_name, s)
+def pad(callback_name, content)
   if(callback_name.length == 0) then
-    return s
+    return content
   else
-    return "#{callback_name}(#{s})"
+    return "#{callback_name}(#{content})"
   end
 end
 
-post '/api/*/*/*' do |callback_name, verify_length, ask_json|
-  if(ask_json.length != verify_length.to_i) then 
-    return "Verify failed.. Your json has #{ask_json.length} letters. We expected #{verify_length.to_i} letters one."
+post '/api/*/*/*' do |callback_name, verify_length, asking_json|
+  if(asking_json.length != verify_length.to_i) then 
+    return "Verify failed.. Your json has #{asking_json.length} letters. We expected #{verify_length.to_i} letters one."
   end
   
-  ask = JSON.parse(ask_json)
+  ask = JSON.parse(asking_json)
   who = ask["who"]
-  md = ask["method"]
+  mthd = ask["method"]
   
   if( ! auth(who[0],who[1])) then return "Wrong email or password" end
   
-  case md[0]
+  case mthd[0]
     when "list" then
-      return pad callback_name,records(md[1],md[2])
+      return pad callback_name,records(mthd[1],mthd[2])
     when "one" then
-      return pad callback_name,record(md[1],md[2])
+      return pad callback_name,record(mthd[1],mthd[2])
     else
   end
 end
 
-get '/signin' do
-  content_type :txt
-  erb :signin
-
+get '/sign_in_gate' do
+  content_type:html
+  erb:sign_in_gate
 end
-
-
-__END__
-
-@@signin
-<h1>Sign-in!</h1>
-  <input id="login_input_uid" type="text" placeholder="email"></input>
-  <input id="login_input_pw" type="password" placeholder="password"></input>
-  <input type="button" onClick="try">サインインする</input>
-
-
-
 
