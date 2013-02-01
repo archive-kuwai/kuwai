bf0b658f74a1278b67569d9fd8c6fbcef6bbb116
naohta <naohta@gmail.com>
2013-02-01 17:08:55 +0900

    Working.. good. But need more refactor.

---
+++ public/js3/NALoadPage.js
@@ -0,0 +1,31 @@
+/**
+ * NALoadPage.js by Naohiro OHTA, All Rights Reserved.
+ */
+
+var NALoadPage = function(){
+
+	// --------------------------------------------
+	// Private members
+	// --------------------------------------------
+    
+	// --------------------------------------------
+	// Public members
+	// --------------------------------------------
+	return{
+        load: function(id, url){
+            var ELEMENT = $("#"+id);
+            ELEMENT.html("");
+            $.ajax({
+              type:"GET",
+              url:url,
+              dataType:"html",
+              success:function(result){ ELEMENT.html(result); },
+              error:function(result){
+                console.log("Error: NALoadPage.js - load()");
+                console.log(result);
+                ELEMENT.html("<h3>ページを読みこんでいる際にエラーが起きました。<br>お手数ですが、もう一度操作を試していただけますか？</h3>");
+              }
+            });
+        }
+    };
+}();
