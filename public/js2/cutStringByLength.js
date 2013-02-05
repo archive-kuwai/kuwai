6a257b167a9b0436471cfea9b7de1ee93a6e791b
naohta <naohta@gmail.com>
2013-02-05 13:50:21 +0900

    A lot of code goes to "kuwai-webapi" project

--- public/js2/cutStringByLength.js
+++
@@ -1,6 +0,0 @@
-function cutStringByLength(str, maxLength){
-	if (str.length > maxLength){
-		str = str.substring(0, maxLength - 1) + "…";
-	}
-	return str;
-}
--- public/js2/shortTime.js
+++
@@ -1,7 +0,0 @@
-function shortTime(d){
-	var h = d.getHours();
-	var m = d.getMinutes();
-	if(h<10){h="0"+h;}
-	if(m<10){m="0"+m;}
-	return h + ":" + m + " ." +d.getSeconds();
-}
